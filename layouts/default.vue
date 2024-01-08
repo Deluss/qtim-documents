4c50fec674e5f950ddb5449e0195abd9069457eb
Andrey Glazkov <deluss21@gmail.com>
2024-01-08 21:53:51 +0300

    test push

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import LayoutHeader from '~/components/layout/LayoutHeader.vue'
+</script>
+
+<template>
+	<el-container>
+		<el-header>
+			<layout-header />
+		</el-header>
+		<el-main>
+			<nuxt-page />
+		</el-main>
+	</el-container>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+	index page
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from '#imports'
+
+onMounted(() => {
+	YaAuthSuggest.init(
+		{
+			client_id: '24aa622ba5d945b7bfefac27e8898eb8',
+			response_type: 'token',
+			redirect_uri: 'https://oauth.yandex.ru/verification_code'
+		},
+		'https://deluss.github.io/qtim-documents'
+	)
+		.then(({
+						 handler
+					 }) => handler())
+		.then(data => console.log('Сообщение с токеном', data))
+		.catch(error => console.log('Обработка ошибки', error));
+})
+</script>
